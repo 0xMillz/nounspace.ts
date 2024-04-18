ef5c4f563acfcd3e4c57287c7eb35e3cd8748ee7
Jesse Paterson <jesse.r.pat@gmail.com>
2024-04-18 00:24:20 -0500

    Simple URL Fidget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,7 +39,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             <Home>
               <Component {...pageProps} />
             </Home>
-          </RainbowKitProvider>
+          </RainbowKitProvider> 
         </QueryClientProvider>
       </WagmiProvider>
     </PostHogProvider>
